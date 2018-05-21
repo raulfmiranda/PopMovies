f28bf5087582e95f7fa6498bc3dc9a95febe4871
Raul Figueira <raulfmiranda@gmail.com>
2018-05-21 19:29:47 -0300

    Criação parcial do xml da tela de login

--- app/src/main/java/com/example/raul/popmovies/LoginActivity.kt
+++ app/src/main/java/com/example/raul/popmovies/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.example.raul.popmovies
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //TODO: https://sourcey.com/beautiful-android-login-and-signup-screens-with-material-design/
+        //TODO: https://medium.com/@pedroimai/dicas-de-arquitetura-em-android-mvp-parte-2-7cda269e6b2b
+        //TODO: https://www.youtube.com/watch?v=7o8jfvMhTSY
+
+        // Trocando fonte do logotipo
+        val typeFace:Typeface = Typeface.createFromAsset(assets, "fonts/true-crimes.ttf")
+        tv_logo.typeface = typeFace
+
+
     }
 }
