34fd781572596220055d2da8bd17658b3663925a
Raul Figueira <raulfmiranda@gmail.com>
2018-05-27 16:54:19 -0300

    Adição de BottomNavigationView e de Fragments na MainActivity

--- app/src/main/java/com/example/raul/popmovies/MainActivity.kt
+++ app/src/main/java/com/example/raul/popmovies/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.raul.popmovies
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar_main)
+
+        addFragment(MostPopFragment(), R.id.frame_fragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_most_popular -> {
+                    toolbar_main.title = getString(R.string.most_popular)
+                    replaceFragment(MostPopFragment(), R.id.frame_fragment)
+                }
+                R.id.action_favorites -> {
+                    toolbar_main.title = getString(R.string.favorites)
+                    replaceFragment(FavoritesFragment(), R.id.frame_fragment)
+                }
+                else -> {
+                    toolbar_main.title = getString(R.string.title_main_activity)
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
     }
-    // TODO: https://medium.com/@hitherejoe/exploring-the-android-design-support-library-bottom-navigation-drawer-548de699e8e0
 }
---
+++ app/src/main/java/com/example/raul/popmovies/extensions.kt
@@ -0,0 +1,26 @@
+package com.example.raul.popmovies
+
+import android.app.Activity
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commit()
+}
+
+fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int){
+    supportFragmentManager.inTransaction { add(frameId, fragment) }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
+    supportFragmentManager.inTransaction{replace(frameId, fragment)}
+}
